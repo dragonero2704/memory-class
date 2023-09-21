92b80e23e25d5e06f42b7caef3032ed0030fa173
dragonero2704 <roberto.rudi04@gmail.com>
2023-09-21 17:38:17 +0200

    sium

--- program.cpp
+++ program.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <Windows.h>
 // #define DEBUG
 
 #include "mem.hpp"
@@ -7,6 +7,7 @@
 #include "offsets.hpp"
 
 using namespace std;
+
 // 0x00007FF74C280000 prefered image base of dummy.exe
 int main(int argc, char** argv) 
 {
@@ -19,17 +20,6 @@ int main(int argc, char** argv)
 		error("Can't get \"%ls\" address", targetProgram.c_str());
 		return EXIT_FAILURE;
 	}
-	/* fails
-	info("Method 1");
-	auto addr1 = mem.Read<uintptr_t>(programBaseAdress + offsets::addr1_offset);
-	ok("%p -> %p", programBaseAdress + offsets::addr1_offset, addr1);
-	auto addr2 = mem.Read<uintptr_t>(addr1 + offsets::addr2_offset);
-	ok("%p -> %p", addr1 + offsets::addr2_offset, addr2);
-	auto addr3 = mem.Read<uintptr_t>(addr2 + offsets::addr3_offset);
-	ok("%p -> %p", addr2 + offsets::addr3_offset, addr3);
-	auto val = mem.Read<int>(addr3 + offsets::addr3_offset_value);
-	ok("%p : %d", addr3 + offsets::addr3_offset_value, val);
-	*/
 	info("Method 2");
 	auto addr1 = mem.Read<uintptr_t>(programBaseAdress + offsets2::addr1_offset);
 	ok("0x%llx -> 0x%llx", programBaseAdress + offsets2::addr1_offset, addr1);
